23459c710b7d356e503797027f8413fbbbb0c6a8
alvin.wang <alvin.wang@binance.com>
2022-05-10 00:19:46 +0200

    chore: virtual module

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import fib from 'virtual:fib';
+
+alert(`结果: ${fib(10)}`)
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import testHooks from './plugins/test-hooks';
+import virtual from './plugins/virtual-module'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), testHooks()]
+  plugins: [react(), virtual()]
 })
