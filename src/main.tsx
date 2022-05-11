65c912ac7d5fee9728e3dfd30145a662b1db1326
alvin.wang <alvin.wang@binance.com>
2022-05-11 23:38:07 +0200

    chore: commit svgr plugin

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import fib from 'virtual:fib';
-import env from 'virtual:env';
+// import fib from 'virtual:fib';
+// import env from 'virtual:env';
 
 
-console.log(env)
+// console.log(env)
 
-alert(`结果: ${fib(10)}`)
+// alert(`结果: ${fib(10)}`)
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite'
 import testHooks from './plugins/test-hooks';
 import virtual from './plugins/virtual-module'
 import react from '@vitejs/plugin-react'
+import svgr from './plugins/svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), virtual()]
+  plugins: [react(), svgr({ defaultExport: 'component' })]
 })
