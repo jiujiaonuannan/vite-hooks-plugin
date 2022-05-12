6b3a42c72d0e7fe89f471469c0236971d41a93f6
alvin.wang <alvin.wang@binance.com>
2022-05-12 22:34:06 +0200

    chore: add inspect plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
-import testHooks from './plugins/test-hooks';
-import virtual from './plugins/virtual-module'
+// import testHooks from './plugins/test-hooks';
+// import virtual from './plugins/virtual-module'
 import react from '@vitejs/plugin-react'
 import svgr from './plugins/svgr';
+import inspect from 'vite-plugin-inspect';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr({ defaultExport: 'component' })]
+  plugins: [react(), svgr({ defaultExport: 'component' }), inspect()]
 })
